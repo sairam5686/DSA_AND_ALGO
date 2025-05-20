51d3507f1ac4973293cbc318299291dbaa964cba
J.R.Sairam <159273258+sairam5686@users.noreply.github.com>
2025-05-21 00:42:19 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (77.29%) - LeetSync

--- 46-permutations/permutations.py
+++ 46-permutations/permutations.py
@@ -1,9 +1,26 @@
-from itertools import permutations
+def permutation(nums , dummy , map ,result):
+    if(len(dummy) == len(nums)):
+        result.append(dummy[:])
+        return
+
+    for i in range(len(nums)):
+        if(map[i] != 1):
+            dummy.append(nums[i])
+            map[i] = 1
+            permutation(nums, dummy, map, result)
+            dummy.pop()
+            map[i] = 0
+
 class Solution(object):
     def permute(self, nums):
         """
         :type nums: List[int]
         :rtype: List[List[int]]
         """
-        return list(permutations(nums))
+        result = []
+        map = [0]*len(nums)
+        permutation(nums,[],map , result)
+        return(result)
+
+        
         
