54b8b29fe9a35fbede606ada2269d451b2fbab0e
J.R.Sairam <159273258+sairam5686@users.noreply.github.com>
2025-05-23 16:39:03 +0530

    Time: 7 ms (80.15%) | Memory: 12.5 MB (36.26%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.py
+++ 40-combination-sum-ii/combination-sum-ii.py
@@ -1,27 +1,22 @@
-def combinationsum(index,arr,dummy,target,result):
-
+def unique_subseq(index , candidates , target , dummy , result):
     if(sum(dummy) == target):
         result.append(dummy[:])
         return
-    for i in range(index,len(arr)):
 
-        if(index<i and arr[i] == arr[i-1]): continue
-        if(target-sum(dummy)<arr[i]): break
-        dummy.append(arr[i])
-        combinationsum(i+1, arr, dummy, target, result)
+    for i in range(index,len(candidates)):
+        if(target-sum(dummy)<candidates[i]):
+            break
+        if(index<i and candidates[i] == candidates[i-1]):
+            continue
+        dummy.append(candidates[i])
+        unique_subseq(i+1, candidates, target, dummy, result)
         dummy.pop()
-
 class Solution(object):
     def combinationSum2(self, candidates, target):
-        """
-        :type candidates: List[int]
-        :type target: int
-        :rtype: List[List[int]]
-        """
         candidates = sorted(candidates)
-
         result = []
-        combinationsum(0 ,candidates ,[] ,target , result )
+        unique_subseq(0, candidates , target,[],result)
         return(result)
+        
         
                 
