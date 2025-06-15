20e7cf2cb8f2432356e3b1d62afd8d458e1e5219
J.R.Sairam <159273258+sairam5686@users.noreply.github.com>
2025-06-15 19:28:01 +0530

    Time: 26 ms (32.26%) | Memory: 12.4 MB (97.47%) - LeetSync

--- 39-combination-sum/combination-sum.py
+++ 39-combination-sum/combination-sum.py
@@ -1,20 +1,19 @@
-def comb_sum(nums , dummy , res , index ,target):
-    if(index == len(nums)):
+def comb_sum(arr,target , index , dummy , result):
+    if(index == len(arr)):
         if(sum(dummy) == target):
-            res.append(dummy[:])
+            result.append(dummy[:])
         return
-    if(sum(dummy)<=target):
-        dummy.append(nums[index])
-        comb_sum(nums, dummy, res, index,target)
+    if(sum(dummy)+arr[index]<=target):
+        dummy.append(arr[index])
+        comb_sum(arr, target , index , dummy , result )
         dummy.pop()
-    comb_sum(nums, dummy, res, index+1,target)
+    comb_sum(arr, target, index+1, dummy, result)
 
 
 
 class Solution(object):
     def combinationSum(self,candidates , target):
         res = []
-        index = 0
-        comb_sum(candidates , [],res , index, target)
-        return (res)
-                
+        comb_sum(candidates, target , 0 , [], res)
+        return(res)
+
