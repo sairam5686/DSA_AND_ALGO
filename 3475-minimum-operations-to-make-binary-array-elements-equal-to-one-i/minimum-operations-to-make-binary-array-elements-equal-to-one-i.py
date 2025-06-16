a5c683ed5f367f8bbdb4adfc5b5679a71271d950
J.R.Sairam <159273258+sairam5686@users.noreply.github.com>
2025-06-16 11:24:55 +0530

    Time: 301 ms (7.56%) | Memory: 15.3 MB (82.35%) - LeetSync

--- 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/minimum-operations-to-make-binary-array-elements-equal-to-one-i.py
+++ 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/minimum-operations-to-make-binary-array-elements-equal-to-one-i.py
@@ -3,22 +3,19 @@ class Solution(object):
         low = 0
         high = 3
         counter = 0
-
         while(high<=len(nums)):
-            if( 0 == nums[low] ):
+            while( 0 == nums[low]):
                 counter +=1
                 for i in range(low, high):
                     if(nums[i] == 0):
                         nums[i] = 1
                     else:
                         nums[i] = 0
-                high +=1
-                low+=1
-            else:
-                low +=1
-                high +=1
+            high +=1
+            low+=1
+            
+            
 
-        print(nums)
         if( 0 in nums):
             return(-1)
         else:
