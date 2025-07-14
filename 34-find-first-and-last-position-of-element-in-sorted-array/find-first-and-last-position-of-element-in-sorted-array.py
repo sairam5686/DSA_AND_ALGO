33f404b3e67291b00986cdddbda0355a5a9db43f
J.R.Sairam <159273258+sairam5686@users.noreply.github.com>
2025-07-14 11:43:20 +0530

    Time: 0 ms (100.00%) | Memory: 13.2 MB (30.93%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,35 +1,26 @@
 class Solution(object):
     def searchRange(self, nums, target):
-        low , high = 0 ,len(nums)-1
-        ind = float('inf')
-        if(len(nums) == 0 ):
-            return([-1,-1])
-        else:
-            while(low<=high):
-                mid  = (low+ high )//2
-                if(nums[mid] == target):
-                    ind = mid
-                    break
-                elif(nums[mid]>target ):
-                    high = mid -1
-                else:
-                    low = mid +1
+        ind =-1
+        low , high = 0 , len(nums)-1
+        while(low <= high):
+            mid = (low + high) // 2
+            if(nums[mid] == target):
+                ind = mid
+                high = mid -1
+            elif(nums[mid]>target):
+                high = mid -1
+            else:
+                low = mid + 1
 
-
-        if(ind == float('inf')):
-            return([-1,-1])
-        else:
-            low, high = ind-1, ind+1
-            while (low>=0 ):
-
-                if(nums[low] == target):
-                    low -= 1
-                else:
-                    break
-
-            while (high<len(nums)):
-                if(nums[high] == target):
-                    high += 1
-                else:
-                    break
-            return([low+1 , high - 1])
+        ind2 =-1
+        low , high = 0 , len(nums)-1
+        while(low <= high):
+            mid = (low + high) // 2
+            if(nums[mid] == target):
+                ind2 = mid
+                low = mid +1
+            elif(nums[mid]>target):
+                high = mid -1
+            else:
+                low = mid + 1
+        return([ind, ind2])
