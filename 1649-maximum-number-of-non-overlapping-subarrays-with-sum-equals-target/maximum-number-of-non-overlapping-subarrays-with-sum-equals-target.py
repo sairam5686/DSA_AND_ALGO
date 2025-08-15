ca20bde78201889c8af8010a87a3f6bc4ab19b0f
J.R.Sairam <159273258+sairam5686@users.noreply.github.com>
2025-08-15 17:40:34 +0530

    Time: 63 ms (50.00%) | Memory: 22.7 MB (33.33%) - LeetSync

---
+++ 1649-maximum-number-of-non-overlapping-subarrays-with-sum-equals-target/maximum-number-of-non-overlapping-subarrays-with-sum-equals-target.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def maxNonOverlapping(self, nums, target):
+        counter = defaultdict(int)
+        counter[0] = 1
+        result = 0
+        prefix_sum = 0
+        
+        for i in range(len(nums)):
+            prefix_sum += nums[i]
+           
+            remainder = prefix_sum - target
+            if remainder in counter:
+                result += 1
+                prefix_sum = 0
+                counter.clear()
+                counter[0] = 1
+            else:
+                counter[prefix_sum]  = 1 
+
+        return result
