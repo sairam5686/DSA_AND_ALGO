7ce724b7274f60add04b8bcb8fea7eb48ccd3b1b
J.R.Sairam <159273258+sairam5686@users.noreply.github.com>
2025-08-15 17:47:33 +0530

    Time: 61 ms (50.00%) | Memory: 22.6 MB (33.33%) - LeetSync

--- 1649-maximum-number-of-non-overlapping-subarrays-with-sum-equals-target/maximum-number-of-non-overlapping-subarrays-with-sum-equals-target.py
+++ 1649-maximum-number-of-non-overlapping-subarrays-with-sum-equals-target/maximum-number-of-non-overlapping-subarrays-with-sum-equals-target.py
@@ -4,17 +4,24 @@ class Solution(object):
         counter[0] = 1
         result = 0
         prefix_sum = 0
-        
         for i in range(len(nums)):
             prefix_sum += nums[i]
-           
+
+            if(prefix_sum == target):
+                result +=1
+                prefix_sum = 0
+                counter.clear()
+                counter[0] =1
+                continue
             remainder = prefix_sum - target
-            if remainder in counter:
-                result += 1
+
+            if(remainder in counter):
+                result +=1
                 prefix_sum = 0
                 counter.clear()
                 counter[0] = 1
             else:
-                counter[prefix_sum]  = 1 
+                counter[prefix_sum] =1
 
-        return result
+
+        return(result)
