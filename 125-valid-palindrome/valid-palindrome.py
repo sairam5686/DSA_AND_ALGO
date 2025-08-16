39efe713c8f443c7d85fe7386d591ffe46ffce66
J.R.Sairam <159273258+sairam5686@users.noreply.github.com>
2025-08-16 20:40:40 +0530

    Time: 135 ms (24.64%) | Memory: 16.3 MB (5.09%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,21 +1,13 @@
 class Solution(object):
-    def isPalindrome(self, s):       
-        dummy = ""
-        for i in s:
-            if(i.isalnum()):
-                dummy  = dummy +i
-        dummy = dummy.lower()
-        if(len(dummy) == 0 or len(dummy) == 1):
-            return True
-        low , high = 0 , len(dummy)-1
-        flag = False
-        while(low<high):
-            if(dummy[low] == dummy[high]):
-                flag = True
-                low +=1
-                high -=1
-            else:
-                return False
-        return flag
-
-        
+    def isPalindrome(self, s):
+        s =  s.lower().strip()
+        ckecher = ""
+        for i in range(len(s)):
+            if(s[i].isalnum()):
+                ckecher +=s[i]
+        if(ckecher == ckecher[::-1]):
+            return(True)
+        else:
+            return(False)
+                
+                
