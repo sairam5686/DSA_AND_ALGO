d3362973e726e7370b060376a073537ec56950dc
J.R.Sairam <159273258+sairam5686@users.noreply.github.com>
2025-08-23 22:54:53 +0530

    Time: 8 ms (51.90%) | Memory: 12.5 MB (12.11%) - LeetSync

---
+++ 12-integer-to-roman/integer-to-roman.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def intToRoman(self, num):
+        symList = [["I", 1], ["IV", 4], ["V", 5], ["IX", 9], ["X", 10], ["XL", 40], ["L", 50], ["XC", 90], ["C", 100], ["CD", 400],["D", 500],["CM", 900],["M", 1000]]
+        res = ""
+        for symbol , value in reversed(symList):
+            if(num//value):
+                count = num//value
+                res += (count*symbol)
+                num = num%value
+        return res
+
+
+        
