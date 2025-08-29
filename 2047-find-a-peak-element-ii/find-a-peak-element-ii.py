18ff479e715db3a4204a3484514ccf1062bcc7fd
J.R.Sairam <159273258+sairam5686@users.noreply.github.com>
2025-08-29 23:36:34 +0530

    Time: 0 ms (100.00%) | Memory: 32.5 MB (17.97%) - LeetSync

--- 2047-find-a-peak-element-ii/find-a-peak-element-ii.py
+++ 2047-find-a-peak-element-ii/find-a-peak-element-ii.py
@@ -1,11 +1,35 @@
+def row_max(mat , col):
+    result , index = 0 ,0
+    for i in range( len(mat)):
+        if(mat[i][col] > result):
+            result =  mat[i][col]
+            index = i
+    return index
+
 class Solution(object):
     def findPeakGrid(self, mat):
-        result = []
-        maxs = 0
-        for i in range(len(mat)):
-            for j in range(len(mat[0])):
-                if(mat[i][j] > maxs):
-                    result = [i,j]
-                    maxs = max(maxs , mat[i][j])
-        return(result)
-                
+        low  , high = 0 , len(mat[0])-1
+        left , right = -1 ,-1
+
+        while(low <=high):
+            mid = (low + high)//2
+            row=row_max(mat , mid)
+
+            if(mid-1<0 ):
+                left  = -1
+            else:
+                left = mat[row][mid-1]
+
+            if (mid +1> len(mat[0])-1):
+                right = -1
+            else:
+                right= mat[row][mid+1]
+
+            if(mat[row][mid]>left and mat[row][mid]>right):
+                return([row,mid])
+              
+            elif(mat[row][mid]< left):
+                high = mid -1
+            else:
+                low = mid + 1
+        return([-1,-1])
