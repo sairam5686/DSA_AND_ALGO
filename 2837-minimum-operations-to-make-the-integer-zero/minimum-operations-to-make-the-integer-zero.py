b9e5e6e144043c4d316763a90c43093ae6392b44
J.R.Sairam <159273258+sairam5686@users.noreply.github.com>
2025-09-05 14:31:00 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (62.03%) - LeetSync

---
+++ 2837-minimum-operations-to-make-the-integer-zero/minimum-operations-to-make-the-integer-zero.py
@@ -0,0 +1,20 @@
+def set_bit(n):
+    n = bin(n)
+    n = n[2:]
+    counter  =  0
+    for i in str(n):
+        if(i == '1'):
+            counter +=1
+    return counter
+
+
+class Solution:
+    def makeTheIntegerZero(self, num1: int, num2: int) -> int:
+        for i in range(0 ,  60 ):
+            target = num1 - i*(num2)
+            if(target < i ):
+                break
+            setter = set_bit(target)
+            if( setter  <= i):
+                return(i)
+        return -1
