66604653c0bab16515313c763e876fb6df41f249
J.R.Sairam <159273258+sairam5686@users.noreply.github.com>
2025-09-17 20:29:25 +0530

    Time: 103 ms (81.00%) | Memory: 51.1 MB (87.07%) - LeetSync

---
+++ 2429-design-a-food-rating-system/design-a-food-rating-system.py
@@ -0,0 +1,31 @@
+class FoodRatings:
+
+    def __init__(self, foods, cuisines, ratings):
+        self.food_info = {}  
+        self.cuisine_heaps = {}  
+
+        for food, cuisine, rating in zip(foods, cuisines, ratings):
+            self.food_info[food] = (cuisine, rating)
+            if cuisine not in self.cuisine_heaps:
+                self.cuisine_heaps[cuisine] = []
+            heapq.heappush(self.cuisine_heaps[cuisine], (-rating, food))
+
+    def changeRating(self, food, newRating):
+        cuisine, _ = self.food_info[food]
+        self.food_info[food] = (cuisine, newRating)
+        heapq.heappush(self.cuisine_heaps[cuisine], (-newRating, food))
+
+    def highestRated(self, cuisine):
+        heap = self.cuisine_heaps[cuisine]
+        while heap:
+            rating_neg, food = heap[0]
+            if self.food_info[food][1] == -rating_neg:
+                return food
+            heapq.heappop(heap)  
+        return ""  
+
+
+# Your FoodRatings object will be instantiated and called as such:
+# obj = FoodRatings(foods, cuisines, ratings)
+# obj.changeRating(food,newRating)
+# param_2 = obj.highestRated(cuisine)
