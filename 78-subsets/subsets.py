dcde7ce73d5ba0600c2dc63f00c1125a813d3328
J.R.Sairam <159273258+sairam5686@users.noreply.github.com>
2025-10-05 18:08:47 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (65.79%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -1,18 +1,17 @@
-def unique_sum(arr , index , dummy , result):
-    if(index == len(arr)):
-        # if(dummy not in result):
-        result.append(dummy[:])
+def subset(a , length ,dummy ,  result):
+    if(len(a) == length):
+        result.append(dummy)
         return
-    dummy.append(arr[index])
-    unique_sum(arr, index+1, dummy, result)
-    dummy.pop()
-    unique_sum(arr,index+1, dummy, result)
 
-class Solution(object):
-    def subsets(self, arr):
-        # arr = [1,2,3]
-        # arr = sorted(arr)
-        res = []
-        unique_sum(arr , 0 , [] , res )
-        return(res)
+
+    dummy.append(a[length])
+    subset(a, length+1,dummy[:] , result)
+    subset(a, length+1, dummy[:-1], result)
+
+
+class Solution:
+    def subsets(self, a: List[int]) -> List[List[int]]:
+        result = []
+        subset(a , 0  , [], result)
+        return(result)
                 
