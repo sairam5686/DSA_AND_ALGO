7652b295cab1654c943759c116a27c20b1108f09
J.R.Sairam <159273258+sairam5686@users.noreply.github.com>
2025-11-03 14:28:34 +0530

    Time: 359 ms (5.05%) | Memory: 19 MB (65.18%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.py
+++ 907-koko-eating-bananas/koko-eating-bananas.py
@@ -1,22 +1,20 @@
-import math
-def hours_taken(piles , eat_rate):
+
+def solver(nums , k):
     total_hours = 0
-    for i in range(len(piles)):
-        total_hours += math.ceil(piles[i] / float(eat_rate))
+    for i in range(len(nums)):
+        total_hours += math.ceil(nums[i] / float(k))
     return total_hours
 
-
-class Solution(object):
-    def minEatingSpeed(self, piles, h):
-        low , high = 1 , max(piles)
-        min_solution = max(piles)
-        while(low<=high ):
-            mid = ((low + high)//2)
-            total = hours_taken(piles,mid)
-            if(total <= h):
-                min_solution = mid
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        solution = -1
+        low , high = 1 , sum(piles)
+        while(low <= high):
+            mid = (low + high ) // 2
+            time_to_eat = solver(piles , mid)
+            if(time_to_eat <= h):
+                solution  = mid
                 high = mid -1
             else:
                 low = mid + 1
-                
-        return min_solution
+        return(solution)
