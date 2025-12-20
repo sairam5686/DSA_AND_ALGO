daeb1aae165d718077945384b917913dd955072f
J.R.Sairam <159273258+sairam5686@users.noreply.github.com>
2025-12-20 14:20:20 +0530

    Time: 0 ms (100.00%) | Memory: 17.2 MB (99.94%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
@@ -1,25 +1,25 @@
-def comb_digit(digits  , length  , dummy , result):
-    dict_number ={
-            1: [], 2: ['a', 'b','c'],3: ['d','e','f'],
-            4: ['g','h','i'],5: ['j','k','l'],     6: ['m','n','o'],
-            7: ['p','q','r','s'], 8: ['t','u','v'],     9: ['w','x','y','z']
-        }
-    
-    if(digits ==""):
-        return []
-
-    if(length == len(digits)):
-        result.append(dummy)
+def permutation(digits , res  , dummy, val):
+    digits_char = {
+        "2": "abc",
+        "3": "def",
+        "4": "ghi",
+        "5": "jkl",
+        "6": "mno",
+        "7": "pqrs",
+        "8": "tuv",
+        "9": "wxyz"
+    }
+    if(val == len(digits)):
+        res.append(dummy)
         return
-    character = digits[length]
-    value_arr = (dict_number[int(character)])
-    for value in value_arr:
-        comb_digit(digits, length+1, dummy+value, result)
+
+    for i in digits_char[digits[val]]:
+        permutation(digits, res, dummy+i, val+1)
+
+
 
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        result = []
-        dummy = ""
-        comb_digit(digits , 0  , dummy , result)
-        return(result)
-                
+        res = []
+        permutation(digits , res  ,  "" , 0)
+        return (res)
