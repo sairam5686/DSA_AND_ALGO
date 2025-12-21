8d413ef21c75e95546a44d73d07e824a790a1ac3
J.R.Sairam <159273258+sairam5686@users.noreply.github.com>
2025-12-21 10:06:51 +0530

    Time: 0 ms (100.00%) | Memory: 17.5 MB (99.07%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -1,17 +1,15 @@
-def Subset(nums , dummy , result , val):
-    if(len(nums)== val):
-        result.append(dummy[:])
+def subset(nums , dummy , res , val):
+    if(val == len(nums)):
+        res.append(dummy[:])
         return
     dummy.append(nums[val])
-    Subset(nums,dummy , result, val+1)
+    subset(nums, dummy, res, val+1)
     dummy.pop()
-    Subset(nums,dummy , result, val+1)
-
-
+    subset(nums, dummy, res, val+1)
 
 
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        dummy  , result , val = []  , [] , 0
-        Subset(nums , dummy , result , val)
-        return(result)
+        result = []
+        subset(nums , [],result , 0  )
+        return (result)
