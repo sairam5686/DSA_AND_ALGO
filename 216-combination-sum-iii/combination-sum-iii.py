2c633685ba1ab457515d2a87dff055869313858f
J.R.Sairam <159273258+sairam5686@users.noreply.github.com>
2025-12-22 15:22:45 +0530

    Time: 0 ms (100.00%) | Memory: 17.4 MB (98.06%) - LeetSync

--- 216-combination-sum-iii/combination-sum-iii.py
+++ 216-combination-sum-iii/combination-sum-iii.py
@@ -1,18 +1,21 @@
-def combination_sum(nums, index , length , target , dummy , result ):
-    if(len(nums) == index):
-        if(sum(dummy) == target and len(dummy) == length):
-            result.append(dummy[:])
+def CombinationSum3(nums  , k , n , dummy , current_sum , res , ind):
+    if(len(dummy) == k and current_sum == n):
+        res.append(dummy[:])
         return
 
-    dummy.append(nums[index])
-    combination_sum(nums, index+1, length, target, dummy, result)
-    dummy.pop()
-    combination_sum(nums, index+1, length, target, dummy, result)
+    for i in range(ind ,  len(nums)):
+        if(i > ind and nums[i] == nums[i-1]):
+            continue
+        if(current_sum > n or nums[i] > n):
+            break
+        dummy.append(nums[i])
+        CombinationSum3(nums, k, n, dummy, current_sum+nums[i], res, i+1)
+        dummy.pop()
+
 
 class Solution:
     def combinationSum3(self, k: int, n: int) -> List[List[int]]:
-        arr_nums = [1,2,3,4,5,6,7,8,9]
-        result = []
-        combination_sum(arr_nums , 0 ,  k , n , [] , result)
-        return(result)
-                
+        nums = [1,2,3,4,5,6,7,8,9]
+        res = [ ]
+        CombinationSum3(nums , k , n , [] , 0  , res , 0 )
+        return(res)
