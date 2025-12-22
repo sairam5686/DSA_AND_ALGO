4bfbd8c5b3f10594b351e66f60e1d496e1b8a2c3
J.R.Sairam <159273258+sairam5686@users.noreply.github.com>
2025-12-22 14:36:38 +0530

    Time: 34 ms (12.80%) | Memory: 17.4 MB (97.82%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.py
+++ 40-combination-sum-ii/combination-sum-ii.py
@@ -1,24 +1,23 @@
-def unique_comb(arr , target, index , dummy , result):
-    if (sum(dummy) == target):
+def CombinationSum2(nums , ind , dummy  , target , result  ):
+    if(sum(dummy) == target):
         result.append(dummy[:])
         return
-    for i in range(index , len(arr)):
-        if (i >index and arr[i-1] == arr[i]):
+
+    for i in range(ind , len(nums)):
+        if(i > ind and nums[i] == nums[i-1]):
             continue
-        if((sum(dummy)+arr[i])>target):
+        if(sum(dummy) > target or nums[i] > target ):
             break
-        dummy.append(arr[i])
-        unique_comb(arr, target, i+1, dummy, result)
+
+
+        dummy.append(nums[i])
+        CombinationSum2(nums, i+1, dummy, target, result )
         dummy.pop()
+    
 
-
-class Solution(object):
-    def combinationSum2(self, candidates, target):
-        candidates = sorted(candidates)
-        res = []
-        unique_comb(candidates, target , 0 , [] , res )
-        return(res)
-
-        
-        
-                
+class Solution:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        candidates.sort()
+        result = []
+        CombinationSum2(candidates,  0  , [] ,  target ,  result ,   )
+        return(result)
