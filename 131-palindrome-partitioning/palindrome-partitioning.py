4e599eaaf6b5d56d4f6bb6d1344266e60e5042ae
J.R.Sairam <159273258+sairam5686@users.noreply.github.com>
2025-12-23 12:14:21 +0530

    Time: 39 ms (88.96%) | Memory: 30 MB (99.86%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.py
+++ 131-palindrome-partitioning/palindrome-partitioning.py
@@ -2,19 +2,14 @@ def PalindromePartition(s, dums  , dummy , result , start ):
     if(start == len(s)):
         result.append(dummy[:])
         return
-
-    for i in range( start , len(s)):
-        # print(s[start : i+1] , dummy  , start , i+1)
-        dums  = s[start : i+1]
-        dums_len = len(dums)
-        if(dums != dums[::-1]):
+    for i in range(start , len(s)):
+        dums = s[start : i+1]
+        if(dums[::-1] != dums):
             continue
-
         dummy.append(dums)
-        dums = ""
-        PalindromePartition(s, dums, dummy, result, start+dums_len)
+        dums=""
+        PalindromePartition(s, dums, dummy, result, i+1)
         dummy.pop()
-
 
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
