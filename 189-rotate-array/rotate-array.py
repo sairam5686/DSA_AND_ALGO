34d14fd5913a4a6ab15a80e27028d4792f7e7cd7
J.R.Sairam <159273258+sairam5686@users.noreply.github.com>
2025-12-24 22:36:54 +0530

    Time: 7 ms (44.30%) | Memory: 24.4 MB (95.36%) - LeetSync

--- 189-rotate-array/rotate-array.py
+++ 189-rotate-array/rotate-array.py
@@ -1,13 +1,23 @@
-def reverse_arr(arr , low , high):
-    while(low<high):
+def reverser(arr , low  , high ):
+    while(low < high):
         arr[low] , arr[high] = arr[high] , arr[low]
         low +=1
         high -=1
 
-class Solution(object):
-    def rotate(self, nums, k):
-        k %=len(nums)
-        reverse_arr(nums, 0 ,len(nums)-1)
-        reverse_arr(nums , 0, k-1)
-        reverse_arr(nums , k,len(nums)-1)
+
+
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        if len(nums) <=1:
+            pass
+        else: 
+
+            if(k > len(nums)):
+                k %= len(nums)
+            reverser(nums , 0 , len(nums)-1)
+            reverser(nums , 0 , k-1)
+            reverser(nums , k , len(nums)-1)
             
