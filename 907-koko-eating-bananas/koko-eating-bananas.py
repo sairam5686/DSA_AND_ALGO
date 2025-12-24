2eac4a42aa8774b281df6dba091be365c22142a5
J.R.Sairam <159273258+sairam5686@users.noreply.github.com>
2025-12-25 01:10:34 +0530

    Time: 179 ms (26.37%) | Memory: 18.8 MB (96.18%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.py
+++ 907-koko-eating-bananas/koko-eating-bananas.py
@@ -1,20 +1,20 @@
-
-def solver(nums , k):
-    total_hours = 0
+def binarysearcher(nums  , k ):
+    res = 0
     for i in range(len(nums)):
-        total_hours += math.ceil(nums[i] / float(k))
-    return total_hours
+        res += (math.ceil(nums[i] / k))
+    return res
+
 
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        solution = -1
-        low , high = 1 , sum(piles)
+        low , high = 1 , max(piles)
+        result  = 0
         while(low <= high):
-            mid = (low + high ) // 2
-            time_to_eat = solver(piles , mid)
-            if(time_to_eat <= h):
-                solution  = mid
+            mid = (low + high )//2
+            res = binarysearcher(piles   ,mid)
+            if(res <= h  ):
+                result = mid
                 high = mid -1
             else:
                 low = mid + 1
-        return(solution)
+        return(result)
