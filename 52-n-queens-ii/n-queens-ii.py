d402729111b62e07b91b1c5084efd4bae60ff7e6
J.R.Sairam <159273258+sairam5686@users.noreply.github.com>
2026-01-20 14:09:34 +0530

    Time: 45 ms (8.73%) | Memory: 19.6 MB (6.08%) - LeetSync

---
+++ 52-n-queens-ii/n-queens-ii.py
@@ -0,0 +1,61 @@
+def checkplace(row , col , board ):
+    for i in range(row-1 , -1 , -1):
+        if(board[i][col] !='.'):
+            return False
+
+    dummy_row = row
+    dummy_col = col
+    while(dummy_row>=0 and dummy_col >=0):
+        if(board[dummy_row][dummy_col] != '.'):
+            return False
+
+        dummy_row -=1
+        dummy_col -=1
+
+    dummy_row = row
+    dummy_col = col
+    while(dummy_row>=0 and dummy_col<len(board[0])):
+        if (board[dummy_row][dummy_col] != '.'):
+            return False
+
+
+        dummy_col +=1
+        dummy_row -=1
+
+    return True
+
+
+
+def NQueen(board , result   , row , no_of_queens):
+    print(board)
+    if(no_of_queens == 0 and board not in result ):
+        result.append(board[:])
+        return
+
+    for col in range( len(board[row])):
+        if( checkplace(row , col , board ) == True):
+            row_list = list(board[row])
+            row_list[col] = 'Q'
+            board[row] = "".join(row_list)
+
+            NQueen(board, result, row+1 ,  no_of_queens-1)
+
+
+            row_list = list(board[row])
+            row_list[col] = '.'
+            board[row] = "".join(row_list)
+
+
+
+
+
+class Solution:
+    def totalNQueens(self, n: int) -> int:
+        result , board = [] ,[]
+        for i in range(n):
+            board.append('.'*n)
+
+
+        row = 0
+        NQueen(board , result  ,  row  , n )
+        return (len(result))
