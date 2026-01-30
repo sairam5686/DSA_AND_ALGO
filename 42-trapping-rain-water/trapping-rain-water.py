512c47ec0518ced49693cfc6a89b9444c43a357e
J.R.Sairam <159273258+sairam5686@users.noreply.github.com>
2026-01-30 13:01:33 +0530

    Time: 15 ms (42.00%) | Memory: 21 MB (34.77%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.py
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -1,18 +1,29 @@
-class Solution(object):
-    def trap(self, nums):
-        l,r,lmax,rmax,total = 0,len(nums)-1,0,0,0
-        while(l<r):
-            if(nums[l]<=nums[r]):
-                if(lmax>nums[l]):
-                    total +=lmax-nums[l]
-                else:
-                    lmax = nums[l]
-                l +=1
+
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        prefixsum  , suffixsum = [0]*len(height)  , [0]*len(height)
+        total , max_element  = 0 , 0
+        for i in range(len(height)):
+            if(i==0 ):
+                max_element = height[i]
+                prefixsum[i] = (height[i])
             else:
-                if(rmax>nums[r]):
-                    total +=rmax-nums[r]
-                else:
-                    rmax = nums[r]
-                r -=1
-        return(total)
-                
+                max_element = max(max_element , height[i])
+                prefixsum[i]=(max_element)
+
+        max_element = 0
+        for i in range(len(height)-1 , -1 , -1):
+            if(i==len(height)-1 ):
+                max_element = height[i]
+                suffixsum[i] =  height[i]
+            else:
+                max_element = max(max_element , height[i])
+                suffixsum[i] = (max_element)
+
+
+
+        for i in range(len(height)):
+       
+            if(height[i] < prefixsum[i] and height[i]<suffixsum[i]):
+                total += (min(prefixsum[i] , suffixsum[i]) - height[i])
+        return (total)
