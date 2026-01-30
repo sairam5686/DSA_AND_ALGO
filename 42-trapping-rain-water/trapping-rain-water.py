31ccf3c70fc80bb00017c5b20646dfed75e7dead
J.R.Sairam <159273258+sairam5686@users.noreply.github.com>
2026-01-30 13:41:01 +0530

    Time: 3 ms (96.45%) | Memory: 21.3 MB (12.40%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.py
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -1,29 +1,19 @@
 
 class Solution:
     def trap(self, height: List[int]) -> int:
-        prefixsum  , suffixsum = [0]*len(height)  , [0]*len(height)
-        total , max_element  = 0 , 0
-        for i in range(len(height)):
-            if(i==0 ):
-                max_element = height[i]
-                prefixsum[i] = (height[i])
+        total , leftmax, rightmax , left = 0 ,0,0 , 0
+        right = len(height)-1
+        while(left < right):
+            if(height[left] <= height[right]):
+                if(height[left] < leftmax ):
+                    total += leftmax - height[left]
+                else:
+                    leftmax = height[left]
+                left +=1
             else:
-                max_element = max(max_element , height[i])
-                prefixsum[i]=(max_element)
-
-        max_element = 0
-        for i in range(len(height)-1 , -1 , -1):
-            if(i==len(height)-1 ):
-                max_element = height[i]
-                suffixsum[i] =  height[i]
-            else:
-                max_element = max(max_element , height[i])
-                suffixsum[i] = (max_element)
-
-
-
-        for i in range(len(height)):
-       
-            if(height[i] < prefixsum[i] and height[i]<suffixsum[i]):
-                total += (min(prefixsum[i] , suffixsum[i]) - height[i])
+                if (height[right] < rightmax):
+                    total += rightmax - height[right]
+                else:
+                    rightmax = height[right]
+                right -= 1
         return (total)
