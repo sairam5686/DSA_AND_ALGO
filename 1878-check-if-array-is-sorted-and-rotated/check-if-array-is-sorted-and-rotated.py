9fc0393d2c48639eb72e74b448a7034d91eefd86
J.R.Sairam <159273258+sairam5686@users.noreply.github.com>
2026-03-05 13:10:00 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (81.19%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.py
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.py
@@ -1,31 +1,22 @@
-class Solution(object):
-    def check(self, nums):
-        res_nums = []
-        low  ,high = 0 , 1
-        bp_1 = True
-        while(high <len(nums)):
-            if(nums[low]<=nums[high]):
-                bp_1 = True
-            else:
-                bp_1 = False
-                break
-            low +=1
-            high +=1
-        if(bp_1 == True):
-            return(True)
-        res_nums +=nums[high::]
-        res_nums +=nums[:low+1:]
+class Solution:
+    def check(self, nums: List[int]) -> bool:
+        if(len(nums) == 1):
+            return True
+        low , high =  0,1
+        bp = 0
+        dums , ind = float('inf') , 0
+        for i in range(1 , len(nums)):
+            if(nums[i] <  nums[i-1]):
+                dums = nums[i]
+                ind = i
 
-        bp_2 = False
-        low , high = 0,1
-        while(high<len(nums)):
-            if(res_nums[low]<=res_nums[high]):
-                bp_2 = True
+        flag = False
+        for i in range(1 , len(nums)):
+            index  = (i+ ind)%len(nums)
+            if(nums[index] >= nums[index-1]):
+                flag = True
             else:
-                bp_2 = False
+                return (False)
                 break
-            low +=1
-            high +=1
 
-        return(bp_2)
-                
+        return(flag)
