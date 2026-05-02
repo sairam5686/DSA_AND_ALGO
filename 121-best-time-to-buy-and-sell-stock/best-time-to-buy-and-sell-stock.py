b44e52eea3571b582fab07e30ec9a0dbfe72a566
J.R.Sairam <159273258+sairam5686@users.noreply.github.com>
2026-05-02 13:10:05 +0530

    Time: 111 ms (36.80%) | Memory: 19.4 MB (13.93%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -1,11 +1,11 @@
-class Solution:
-    def maxProfit(self, prices: List[int]) -> int: 
-        stack = [0]
-        max_profit =  0
-        for i in range(len(prices)-1 , -1 , -1):
-            if(stack[-1]< prices[i] ):
-                stack.append(prices[i])
+class Solution(object):
+    def maxProfit(self, nums):
+        stack  = [0]
+        max_profit =   0 
+        for i in range(len(nums)-1 , -1 , -1):
+            if(stack[-1] < nums[i]):
+                stack.append(nums[i])
             else:
-                profit =  stack[-1] - prices[i]
-                max_profit  = max(max_profit, profit)
-        return(max_profit)
+                profit = stack[-1] - nums[i]
+                max_profit = max(profit  , max_profit)
+        return max_profit
