bd44756d9692cd39435ab0a3c24b15e58c673cb5
J.R.Sairam <159273258+sairam5686@users.noreply.github.com>
2026-08-09 13:01:13 +0530

    Time: 3 ms (81.55%) | Memory: 20.6 MB (24.48%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -3,14 +3,11 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        low , high = 0 ,  0
-        while(high < len(nums)):
-            if(nums[high] != 0):
+        low , high = 0  , 0
+        while high < len(nums):
+            if(nums[high] != 0  ):
                 nums[low] , nums[high] = nums[high] , nums[low]
                 low +=1
-                high +=1
-            else:
-                high +=1
-                
-        
-                
+            high +=1
+
+
