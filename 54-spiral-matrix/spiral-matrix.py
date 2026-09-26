c1b42f9cce2be3c2c4ac9aadae6522db9a97fc54
J.R.Sairam <159273258+sairam5686@users.noreply.github.com>
2026-09-26 19:39:20 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (35.59%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.py
+++ 54-spiral-matrix/spiral-matrix.py
@@ -1,25 +1,29 @@
 class Solution:
-    def spiralOrder(self, nums: list[list[int]]) -> list[int]:
+    def spiralOrder(self, matrix: list[list[int]]) -> list[int]:
+        left =  0
+        right  = len(matrix[0])
+        top  = 0
+        bottom = len(matrix)
         res = []
-        top , down , left , right =  0 ,len(nums)-1 , 0 , len(nums[0])-1
-        while(top <= down and left <=right):
-            for i in range(left , right+1):
-                res.append(nums[top][i])
-            top+=1
+        while(top < bottom and left<right):
+            for i in range(left , right):
+                print(top ,  i)
+                res.append(matrix[top][i])
 
-            for i in range(top , down+1):
-                res.append(nums[i][right])
+            top +=1
+            for i in range(top , bottom):
+                res.append(matrix[i][right-1])
 
             right -=1
 
-            if (top <=down):
-                for i in range(right , left-1 ,-1):
-                    res.append(nums[down][i])
+            if (top < bottom):
+                for i in range(right-1  , left-1 , -1):
+                    res.append(matrix[bottom-1][i])
 
-                down-=1
+                bottom -=1
 
-            if(left <=right):
-                for i in range(down, top-1 , -1):
-                    res.append(nums[i][left])
+            if(left <right):
+                for i in range(bottom-1, top-1, -1):
+                    res.append(matrix[i][left])
                 left +=1
         return(res)
